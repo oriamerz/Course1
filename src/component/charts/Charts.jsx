a3ce6dfc94905df7f7f3e37fe826ca5f75786065
oriamerz <oriamerz@gmail.com>
2021-02-03 18:54:38 +0200

    chenge backgroundColor in chart

--- src/component/charts/Charts.jsx
+++ src/component/charts/Charts.jsx
@@ -34,7 +34,7 @@ const Chart = ({data, country}) => {
                     data: dailyData.map(({ deaths }) => deaths),
                     label: 'Deaths',
                     borderColor: 'red',
-                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    backgroundColor: 'rgba(254, 0, 0, 0.5)',
                     fill: true,
                 }]
             }} />) : null
